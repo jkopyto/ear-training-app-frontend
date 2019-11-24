5729b2fcefb21609f935a1785b4ca1e6851f7e59
Jakub Kopyto <j.kopyto@iambot.ai>
2019-11-24 22:39:47 +0100

    Added new enharmonic excersise

--- src/components/EnharmonicExcersises/excersises.ts
+++ src/components/EnharmonicExcersises/excersises.ts
@@ -6,5 +6,10 @@ const excersise1:EnharmonicExcersise = {
     backendTitle: "First_enh_excersise"
 }
 
+const excersise2: EnharmonicExcersise = {
+    sheetBackendTitle: "Bach_invention_13",
+    title: "Bach: Invention 13 in A Minor, BWv 784",
+    backendTitle: "Bach_invention_13"
+}
 
-export const Excersises: EnharmonicExcersise[] = [excersise1]
+export const Excersises: EnharmonicExcersise[] = [excersise1,excersise2]
